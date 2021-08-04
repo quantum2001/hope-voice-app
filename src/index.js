9273a212c90df25ec4c20edc3916ecf54057a950
Samuel Lawal <samuellawal1979@gmail.com>
2021-08-04 16:21:19 +0100

    done the first part of the inventory

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Iventory from './Iventory';
+import './inventory.css'
+import NavBar from './NavBar'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Switch>
+        <Route exact path='/'>
+          <div className='container'>
+            <NavBar />
+          </div>
+        </Route>
         <Route path='/inventory'>
           <Iventory />
         </Route>
+
       </Switch>
     </Router>
 
